76f053e0f3e1a693193ab9a27e00b80298b74495
Riccardo Schöpf <rischoepf38@gmail.com>
2025-05-14 00:46:20 +0200

    menu 1.0

---
+++ styles/template/menu.js
@@ -0,0 +1,29 @@
+function menu() {
+    return `    <div class="menu-style">
+        <div class="flex-center">
+            <img class="logo-join" src="./svg/join-logo.png" alt="join logo">
+        </div>
+        <div>
+            <a href="#summary" class="flex-center margin-bottom-10px">
+                <img src="./svg/summary.png" alt="summary logo">
+                <p class="padding-left-10px">Summary</p>
+            </a>
+            <a href="#add-task" class="flex-center margin-bottom-10px">
+                <img src="./svg/add-task.png" alt="add task logo">
+                <p class="padding-left-10px">Add Task</p>
+            </a>
+            <a href="#board" class="flex-center margin-bottom-10px">
+                <img src="./svg/board.png" alt="board logo">
+                <p class="padding-left-10px">Board</p>
+            </a>
+            <a href="#contact" class="flex-center margin-bottom-10px">
+                <img src="./svg/contact.png" alt="contact logo">
+                <p class="padding-left-10px">Contact</p>
+            </a>
+        </div>
+        <div class="possition-absolute-bottom">
+            <a href="#privacy-policy" class="flex-center padding-10px">Privacy Policy</a>
+            <a href="#legal-notice" class="flex-center padding-10px">Legal notice</a>
+        </div>
+    </div>`;
+}
