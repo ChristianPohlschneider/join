f936cb8406301707b62b24bee230640273cb1b1b
Riccardo Schöpf <rischoepf38@gmail.com>
2025-05-14 01:20:17 +0200

    add png

--- styles/template/menu.js
+++ styles/template/menu.js
@@ -1,23 +1,23 @@
 function menu() {
     return `    <div class="menu-style">
         <div class="flex-center">
-            <img class="logo-join" src="./svg/join-logo.png" alt="join logo">
+            <img class="logo-join" src="./assets/icons/join-logo.png" alt="join logo">
         </div>
         <div>
-            <a href="#summary" class="flex-center margin-bottom-10px">
-                <img src="./svg/summary.png" alt="summary logo">
+            <a href="#summary" class="menu-button-possition margin-bottom-10px">
+                <img src="./assets/icons/summary.png" alt="summary logo">
                 <p class="padding-left-10px">Summary</p>
             </a>
-            <a href="#add-task" class="flex-center margin-bottom-10px">
-                <img src="./svg/add-task.png" alt="add task logo">
+            <a href="#add-task" class="menu-button-possition margin-bottom-10px">
+                <img src="./assets/icons/add-task.png" alt="add task logo">
                 <p class="padding-left-10px">Add Task</p>
             </a>
-            <a href="#board" class="flex-center margin-bottom-10px">
-                <img src="./svg/board.png" alt="board logo">
+            <a href="#board" class="menu-button-possition margin-bottom-10px">
+                <img src="./assets/icons/board.png" alt="board logo">
                 <p class="padding-left-10px">Board</p>
             </a>
-            <a href="#contact" class="flex-center margin-bottom-10px">
-                <img src="./svg/contact.png" alt="contact logo">
+            <a href="#contact" class="menu-button-possition margin-bottom-10px">
+                <img src="./assets/icons/contact.png" alt="contact logo">
                 <p class="padding-left-10px">Contact</p>
             </a>
         </div>
