0d3ce55f44648f5e85ffd023544e093bb4ac79a2
Christian Pohlschneider <c.pohlschneider@t-online.de>
2025-05-14 19:27:40 +0200

    implemented menu to board

--- assets/templates/menu.js
+++ assets/templates/menu.js
@@ -1,23 +1,23 @@
 function menu() {
     return `    <div class="menu-style">
         <div class="flex-center">
-            <img class="logo-join" src="./assets/icons/join-logo.png" alt="join logo">
+            <img class="logo-join" src="../assets/icons/join-logo.png" alt="join logo">
         </div>
         <div>
             <a href="#summary" class="menu-button-possition margin-bottom-10px">
-                <img src="./assets/icons/summary.png" alt="summary logo">
+                <img src="../assets/icons/summary.png" alt="summary logo">
                 <p class="padding-left-10px">Summary</p>
             </a>
             <a href="#add-task" class="menu-button-possition margin-bottom-10px">
-                <img src="./assets/icons/add-task.png" alt="add task logo">
+                <img src="../assets/icons/add-task.png" alt="add task logo">
                 <p class="padding-left-10px">Add Task</p>
             </a>
             <a href="#board" class="menu-button-possition margin-bottom-10px">
-                <img src="./assets/icons/board.png" alt="board logo">
+                <img src="../assets/icons/board.png" alt="board logo">
                 <p class="padding-left-10px">Board</p>
             </a>
             <a href="#contact" class="menu-button-possition margin-bottom-10px">
-                <img src="./assets/icons/contact.png" alt="contact logo">
+                <img src="../assets/icons/contact.png" alt="contact logo">
                 <p class="padding-left-10px">Contact</p>
             </a>
         </div>
--- js/board.js
+++ js/board.js
@@ -1,3 +1,4 @@
 function initboard () {
+    document.getElementById("menuTemplate").innerHTML = menu();
     document.getElementById("boardTemplate").innerHTML = initBoardTemplate();
 }
