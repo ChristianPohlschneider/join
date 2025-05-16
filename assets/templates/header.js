5e319c94b71638feaa7cb4457e4fff3fe8be997d
Fabian Bartholdi <fafa.fsb@gmail.com>
2025-05-16 22:49:26 +0200

    header-style

---
+++ assets/templates/header.js
@@ -0,0 +1,18 @@
+function header() {
+    return `  
+    <div class="header-title">Kanban Project Management Tool</div>
+    <div class="header-help">
+        <a href="../html/help.html">
+            <img src="../assets/icons/help.png" alt="Help Icon" class="help-icon">
+        </a>
+            <div id="userAvatar" class="avatar"></div>
+            <div id="userSlider" class="slider">
+                <ul>
+                    <li><a href="./legal_notice.html">Legal Notice</a></li>
+                    <li><a href="./privacy_policy.html">Privacy Police</a></li>
+                    <li><a href="./sign_up.html">Log out</a></li>
+                </ul>
+            </div>
+    </div>
+    `;
+}
