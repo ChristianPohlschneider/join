eb22e15a6ad1216db32fc6fb4acd09798d3ec964
Fabian Bartholdi <fafa.fsb@gmail.com>
2025-05-16 21:19:23 +0200

    header-slider

--- js/add_task.js
+++ js/add_task.js
@@ -2,4 +2,19 @@ function initTask () {
     document.getElementById("menuTemplate").innerHTML = menu();
 }
 
+const avatar = document.getElementById("userAvatar");
+const slider = document.getElementById("userSlider");
+
+avatar.addEventListener("click", (e) => {
+  e.stopPropagation();
+  slider.classList.toggle("open");
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInside = slider.contains(e.target) || avatar.contains(e.target);
+  if (!isClickInside) {
+    slider.classList.remove("open");
+  }
+});
+
 
