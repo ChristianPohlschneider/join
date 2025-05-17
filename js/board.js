84966fece04d6e8ab84e5df556ec256e7652910b
ChristianPohlschneider <c.pohlschneider@t-online.de>
2025-05-17 18:53:16 +0200

    init header board

--- js/board.js
+++ js/board.js
@@ -1,4 +1,6 @@
 function initboard () {
-    document.getElementById("menuTemplate").innerHTML = menu();
+    // document.getElementById("menuTemplate").innerHTML = menu();
     // document.getElementById("boardTemplate").innerHTML = initBoardTemplate();
+        document.getElementById("menuTemplate").innerHTML = menu();
+    document.getElementById("headerTemplate").innerHTML = header();
 }
