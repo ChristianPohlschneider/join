9d0f47471d89bcb765fe95ea90905b6f81f821e3
Fabian Bartholdi <fafa.fsb@gmail.com>
2025-05-18 22:30:46 +0200

    header-add-_task

--- js/add_task.js
+++ js/add_task.js
@@ -1,25 +1,2 @@
-function initTask () {
-  document.getElementById("menuTemplate").innerHTML = menu();
-  document.getElementById("headerTemplate").innerHTML = header();
-
-  initAvatarSlider();
-}
-
-function initAvatarSlider() {
-  const avatar = document.getElementById("userAvatar");
-  const slider = document.getElementById("userSlider");
-
-    avatar.addEventListener("click", (e) => {
-      e.stopPropagation();
-      slider.classList.toggle("open");
-    });
-
-    document.addEventListener("click", (e) => {
-      const isClickInside = slider.contains(e.target) || avatar.contains(e.target);
-      if (!isClickInside) {
-        slider.classList.remove("open");
-      }
-    });
-}
 
 
