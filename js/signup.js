2d0b6c72b6672da054f4f5a745ba0b4f7d968f22
Christopher Hipper <hipper.christopher@gmail.com>
2025-05-18 16:36:42 +0200

    clean code and toggleCheckbox

--- js/signup.js
+++ js/signup.js
@@ -1,32 +1,37 @@
+let checkbox = false;
+
 async function signup() {
-    let email = document.getElementById('mail');
-    let password = document.getElementById('password');
-    let confirmPwassword = document.getElementById('confirm-password');
-    let name = document.getElementById('name');
     try {
-        checkName(name);
-        checkMail(email);
-        checkPassword(password, confirmPwassword);
+        clearErrors();
+        checkName();
+        checkMail();
+        checkPassword();
     } catch (error) {
-        if (error.code === 'password mismatch') {
-            error.passwordText.classList.remove('d_none');
-            error.passwordBorder.classList.add('red-border');
-            error.passwordConfirmBorder.classList.add('red-border');
-        } else if (error.code === 'noName') {
-            error.nameText.classList.remove('d_none');
-            error.nameBorder.classList.add('red-border');
-        } else {
-            error.mailText.classList.remove('d_none');
-            error.mailBorder.classList.add('red-border');
-        };
-    };
+        error.code === 'password mismatch' ? passwordError(error) : error.code === 'noName' ? nameError(error) : emailError(error);
+    }
 };
 
-function checkName(name) {
+function clearErrors() {
     let nameContainer = document.getElementById('name-input-container');
-    let nameInput = document.getElementById('warning-name')
+    let nameInput = document.getElementById('warning-name');
     nameInput.classList.add('d_none');
     nameContainer.classList.remove('red-border');
+    let matchPassword = document.getElementById('warning-password');
+    let passwordContainer = document.getElementById('pasword-confirm-input-container');
+    let passwordCOnfirmContainer = document.getElementById('pasword-input-container');
+    matchPassword.classList.add('d_none');
+    passwordContainer.classList.remove('red-border');
+    passwordCOnfirmContainer.classList.remove('red-border');
+    let warningMail = document.getElementById('warning-mail');
+    let mailContainer = document.getElementById('mail-input-container');
+    warningMail.classList.add('d_none');
+    mailContainer.classList.remove('red-border');
+};
+
+function checkName() {
+    let name = document.getElementById('name');
+    let nameContainer = document.getElementById('name-input-container');
+    let nameInput = document.getElementById('warning-name');
     if (name.value.trim().length === 0) {
         throw {
             code: 'noName',
@@ -38,14 +43,13 @@ function checkName(name) {
     };
 };
 
-function checkPassword(password, confirmPassword) {
+function checkPassword() {
+    let password = document.getElementById('password');
+    let confirmPwassword = document.getElementById('confirm-password');
     let matchPassword = document.getElementById('warning-password');
     let passwordContainer = document.getElementById('pasword-confirm-input-container');
     let passwordCOnfirmContainer = document.getElementById('pasword-input-container');
-    matchPassword.classList.add('d_none');
-    passwordContainer.classList.remove('red-border');
-    passwordCOnfirmContainer.classList.remove('red-border');
-    if (password.value === confirmPassword.value && password.value.length != 0) {
+    if (password.value === confirmPwassword.value && password.value.length != 0) {
         return
     } else {
         throw {
@@ -57,11 +61,10 @@ function checkPassword(password, confirmPassword) {
     };
 };
 
-function checkMail(email) {
+function checkMail() {
+    let email = document.getElementById('mail');
     let warningMail = document.getElementById('warning-mail');
     let mailContainer = document.getElementById('mail-input-container');
-    warningMail.classList.add('d_none')
-    mailContainer.classList.remove('red-border')
     if (!email.value.includes('@') || !email.value.includes('.') || email.value.trim().length === 0) {
         throw {
             code: 'email incorrect',
@@ -99,4 +102,33 @@ function togglePasswordVisibility(inputId, iconId) {
     } else {
         return
     };
+};
+
+function passwordError(error) {
+    error.passwordText.classList.remove('d_none');
+    error.passwordBorder.classList.add('red-border');
+    error.passwordConfirmBorder.classList.add('red-border');
+};
+
+function nameError(error) {
+    error.nameText.classList.remove('d_none');
+    error.nameBorder.classList.add('red-border');
+};
+
+function emailError(error) {
+    error.mailText.classList.remove('d_none');
+    error.mailBorder.classList.add('red-border');
+};
+
+function toggleCheckbox() {
+    let checkboxRef = document.getElementById('checkbox');
+    let signupBtn = document.getElementById('signup-Btn');
+    if (checkbox == false) {
+        checkboxRef.src = `../assets/icons/checkbox-checked.png`
+        signupBtn.disabled = false;
+    } else {
+        checkboxRef.src = `../assets/icons/checkbox.png`;
+        signupBtn.disabled = true;
+    }
+    checkbox = !checkbox;
 };
