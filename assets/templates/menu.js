73d7c61516c10f5dab326cb4de29962c74083353
Christopher Hipper <hipper.christopher@gmail.com>
2025-05-21 21:13:53 +0200

    start resp menu

--- assets/templates/menu.js
+++ assets/templates/menu.js
@@ -1,6 +1,5 @@
 function menuLogged() {
-    return `    
-    <div class="menu-style">
+    return `    <div class="menu-style">
         <div class="menu-tasks">
             <div class="menu-div">
                 <img class="logo-join" src="../assets/icons/join-logo.png" alt="join logo">
@@ -29,7 +28,29 @@ function menuLogged() {
             <a href="../html/privacy_policy.html" class="flex-center padding-10px">Privacy Policy</a>
             <a href="../html/legal_notice.html" class="flex-center padding-10px">Legal notice</a>
         </div>
-    </div>
+    </div>    
+    `;
+}
+
+function mobileMenuLogged() {
+    return `    <div class="mobile-menu-container">
+        <a href="./summary.html">
+            <img src="../assets/icons/summary.png" alt="">
+            <p>Summary</p>
+        </a>
+        <a href="./board.html">
+            <img src="../assets/icons/board.png" alt="">
+            <p>Board</p>
+        </a>
+        <a href="./add_task.html">
+            <img src="../assets/icons/add-task.png" alt="">
+            <p>Add Tasks</p>
+        </a>
+        <a href="./contacts.html">
+            <img src="../assets/icons/contact.png" alt="">
+            <p>Contacts</p>
+        </a>
+    </div>    
     `;
 }
 
@@ -58,8 +79,8 @@ function menuUnlogged() {
 function checkLogged() {
     let userName = localStorage.getItem("user");
     if (userName == undefined) {
-        return menuUnlogged()
+        return menuUnlogged();
     } else {
-        return menuLogged()
+        return menuLogged();
     }
 }
