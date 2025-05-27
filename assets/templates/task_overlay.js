acf800986dda1d60f4a09e3b72f91e15e98a7df0
ChristianPohlschneider <c.pohlschneider@t-online.de>
2025-05-27 20:11:00 +0200

    render task overlay

--- assets/templates/task_overlay.js
+++ assets/templates/task_overlay.js
@@ -1,19 +1,28 @@
 function renderTaskOverlay(taskIndex) {
     return `
         <div id="cardOverlay" class="cardOverlay">
-            <div id="taskCategory${"#" + taskIndex}" class="taskOverlayCategory">${tasks[taskIndex].category}<div>&times;</div></div>
+            <div class="taskCategoryDiv"><div id="taskCategory${"#" + taskIndex}" class="taskOverlayCategory">${tasks[taskIndex].category}</div><div>&times;</div></div>
             <div id="taskName" class="taskOverlayName">${tasks[taskIndex].name}</div>
             <div id="taskDescription" class="taskOverlayDescription">${tasks[taskIndex].description}</div>
-            <div id="subtasks${"#" + taskIndex}" class="overlaySubtasks">
-                <div id="outerScale${"#" + taskIndex}" class="outerScaleOverlay">
-                    <div class="innerScale" id="innerScaleOverlay${"#" + taskIndex}"></div>
-                </div>
-                <p id="subtaskDone${"#" + taskIndex}" class="subtaskOverlayDone"></p>
+            <p>Due date:</p>
+            <div class="priorityOverlay">
+                <p>Priority:</p>
+                <div id="priority${"#" + taskIndex}"></div>
             </div>
             <div id="taskAssignment" class="taskOverlayAssignment">
+                <p>Assigned To:</p>
                 <div id="assignedTo${"#" + taskIndex}" class="assignedToOverlay"></div>
-                <div id="priority${"#" + taskIndex}" class="priorityOverlay"></div>
+            </div>
+            <p>Subtasks</p>
+            <div id="subtasks${"#" + taskIndex}" class="overlaySubtasks">
+
             </div>
         </div>
+    `;
+}
+
+function renderOverlaySubtasks(taskIndex, subtaskIndex) {
+    return `
+        <input type="checkbox" name="" id="overlaySubtask${"#" + taskIndex + "#" + subtaskIndex}"></input>
     `;
 }
--- js/overlay.js
+++ js/overlay.js
@@ -1,6 +1,25 @@
 function loadTaskOverlayData(taskIndex) {
     findBackgroundColor(taskIndex);
-    getSubtasks(taskIndex);
+    getOverlaySubtasks(taskIndex);
     getAssignedTo(taskIndex);
     getPriority(taskIndex);
+}
+
+function getOverlaySubtasks(taskIndex) {
+    if (tasks[taskIndex].subtasks === undefined) {
+            document.getElementById("subtasks#" + taskIndex).style.display = "none";
+    }
+    getSubtaskIndex(taskIndex);
+    for (let subtaskIndex = 0; subtaskIndex < subtaskArray.length; subtaskIndex++) {
+        if (subtaskArray[subtaskIndex].done == true) {
+            // check checkbox
+            // render subtask
+            document.getElementById("subtasks#" + taskIndex).innerHTML += renderOverlaySubtasks(taskIndex, subtaskIndex);
+        } else {
+            // uncheck checkbox
+            // render subtask
+            document.getElementById("subtasks#" + taskIndex).innerHTML += renderOverlaySubtasks(taskIndex, subtaskIndex);
+        }
+    }
+    subtaskArray = [];
 }
