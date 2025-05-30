0a9a8a2199c2928441cd6ebad1e13b60eb624f53
ChristianPohlschneider <c.pohlschneider@t-online.de>
2025-05-30 21:28:20 +0200

    task overlay input and css

--- assets/templates/task_overlay.js
+++ assets/templates/task_overlay.js
@@ -1,24 +1,30 @@
 function renderTaskOverlay(taskIndex) {
     return `
         <div id="cardOverlay" class="cardOverlay">
-            <div class="taskCategoryDiv"><div id="taskCategory${"#" + taskIndex}" class="taskOverlayCategory">${tasks[taskIndex].category}</div><div onclick="closeOverlay()">&times;</div></div>
+            <div class="taskCategoryDiv"><div id="taskCategory${"#" + taskIndex}" class="taskOverlayCategory">${tasks[taskIndex].category}</div><div class="closeImg" onclick="closeOverlay()"></div></div>
             <div id="taskName" class="taskOverlayName">${tasks[taskIndex].name}</div>
             <div id="taskDescription" class="taskOverlayDescription">${tasks[taskIndex].description}</div>
             <div class="dueDateOverlay">   
-                <p>Due date:</p>
+                <p class="fontSize20">Due date:</p>
                 <div class="dueDateOverlayDiv">${tasks[taskIndex].date}</div>
             </div>
             <div class="priorityOverlay">
-                <p>Priority:</p>
+                <p class="fontSize20">Priority:</p>
                 <div id="priority${"#" + taskIndex}" class="priorityOverlayDiv"></div>
             </div>
             <div id="taskAssignment" class="taskOverlayAssignment">
-                <p>Assigned To:</p>
+                <p class="fontSize20">Assigned To:</p>
                 <div id="assignedTo${"#" + taskIndex}" class="assignedToOverlay"></div>
             </div>
-            <p id="subTaskHeadTitle">Subtasks</p>
             <div id="subtasks${"#" + taskIndex}" class="overlaySubtasks">
-
+                <p id="subTaskHeadTitle" class="subTaskHeadTitle">Subtasks</p>
+            </div>
+            <div class="editDeleteDiv">
+                <img class="editDeleteImg" src="../assets/icons/delete.png" alt="delete">
+                <div>Delete</div>
+                <img class="editDeleteVector" src="../assets/icons/vector.png" alt="vector">
+                <img class="editDeleteImg" src="../assets/icons/edit-v2.png" alt="edit">
+                <div>Edit</div>
             </div>
         </div>
     `;
@@ -26,8 +32,13 @@ function renderTaskOverlay(taskIndex) {
 
 function renderOverlaySubtasks(taskIndex, subtaskIndex, subtaskCounter) {
     return `
-        <input type="checkbox" name="" id="overlaySubtask${"#" + taskIndex + "#" + subtaskCounter}"></input>
-        <div id="#" class="">${subtaskArray[subtaskIndex].title}</div>
+        <div class="overlaySubtaskDiv">
+            <label class="overlaySubtaskCheckbox">
+            <input type="checkbox" name="" id="overlaySubtask${"#" + taskIndex + "#" + subtaskCounter}" class="overlaySubtaskInput"></input>
+            <span class="overlaySubtaskCheckmark"></span>
+            </label>
+            <div id="#" class="">${subtaskArray[subtaskIndex].title}</div>
+        </div>
     `;
 }
 
