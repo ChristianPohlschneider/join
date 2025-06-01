f4172374eb4d8d25af1b4bafe7befba009fecfbc
Christopher Hipper <hipper.christopher@gmail.com>
2025-06-01 15:19:11 +0200

    toggle assignedTo

--- js/add_task.js
+++ js/add_task.js
@@ -320,3 +320,11 @@ function addEditSubtask(index) {
     renderSubtasks();
 
 }
+
+document.addEventListener('click', e => {
+    let assignInput = document.getElementById('assignee-input')
+    let assignedRef = document.getElementById('assigned')
+    if(!assignedRef.contains(e.target) && e.target !== assignInput){
+        assignedRef.classList.add('dnone')
+    }
+})
